50b37b915a2e74e8b12ef66689bf2a246412d482
gahag <gabriel.s.b@live.com>
2017-06-11 00:49:55 -0300

    Updated ps1.

--- ps1.sh
+++ ps1.sh
@@ -1,1 +1,21 @@
-PS1='[\u@\h \W]> '
+PROMPT_COMMAND=__prompt_command # Func to gen PS1 after CMDs
+
+__prompt_command() {
+  local EXIT="$?" # This needs to be first
+  
+  local RCol='\[\e[0m\]'
+  
+  local Red='\[\e[0;31m\]'
+  local Gre='\[\e[0;32m\]'
+  local BYel='\[\e[1;33m\]'
+  local BBlu='\[\e[1;34m\]'
+  local Pur='\[\e[0;35m\]'
+  
+  PS1="${BBlu}[${RCol}\u "
+  
+  if [ $EXIT != 0 ]; then
+    PS1+="${Red}$EXIT "
+  fi
+
+  PS1+="${BYel}\W${BBlu}]>${RCol} "
+}
