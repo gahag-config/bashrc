bb642d5b1b3bf989f4bcb6328384eb071622cbad
gahag <gabriel.s.b@live.com>
2017-08-04 03:10:39 -0300

    updated ls

--- system.sh
+++ system.sh
@@ -1,4 +1,4 @@
-alias ls='ls --color=auto'
+alias ls='ls --color=auto -lh --time-style long-iso'
 
 function partbkp { # $1 : Partition ID (sdXY) ; $2 : Output file
   sudo dd if="/dev/$1" conv=sync,noerror bs=64K "of=$2"
