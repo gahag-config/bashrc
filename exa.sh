3faff3bc376566e5c6034a85bb3426dc8b4d56c6
gahag <gabriel.s.b@live.com>
2018-08-29 00:24:44 -0300

    Removed `a` switch from exa.

--- exa.sh
+++ exa.sh
@@ -1,3 +1,3 @@
 export EXA_COLORS="ur=30:uw=30:ux=1;33:ue=1;33:gr=30:gw=30:gx=1;33:tr=30:tw=30:tx=1;33:su=34:sf=34:xa=34:sn=30:sb=30:da=30:uu=30:un=1;30:gu=30:gn=1;30"
 
-alias lx="exa -la --group-directories-first --time-style long-iso"
+alias lx="exa -l --group-directories-first --time-style long-iso"
