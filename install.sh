85f7a3c8d752c75ef84b76da2a021819384498ce
gahag <gabriel.s.b@live.com>
2019-01-07 01:21:02 -0200

    Added histfile setting.

--- install.sh
+++ install.sh
@@ -3,3 +3,9 @@
 cd $(dirname $(readlink -f $0)) # jump to the script directory
 
 ln -s "$(pwd)/bashrc" ~/.bashrc
+
+if [ -z "$HISTFILE" ]; then
+    echo "HISTFILE not defined, skipping...";
+else
+  mkdir -p "$(dirname $HISTFILE)"
+fi
