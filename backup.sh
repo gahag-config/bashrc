79f475a194bcdfa26e22630d75985b84a6459355
gahag <gabriel.s.b@live.com>
2019-08-26 19:16:21 -0300

    Added gahag-backup function. Renamed old gahag-backup to gahag-replica.

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 # }
 
 
-function gahag-backup {
+function gahag-replica {
   # add --delete flag to delete extraneous files from the
   # receiving side (ones that aren’t on the sending side).
   rsync -av --delete --delete-excluded                                \
@@ -17,4 +17,22 @@ function gahag-backup {
         --exclude='gahag/programming/projects/ic/DetuxUbuntu-1.0.ova' \
         /gahag "/run/media/gahag/gahag - portable/backup/" 2>&1       \
   | tee $(date '+%Y-%m-%d(%H:%M:%S)-rsync.log')
+}
+
+function gahag-backup {
+  tar -cvpzf "/run/media/gahag/Backup/backup.$(date '+%Y-%m-%d').tar.gz" \
+      --exclude='*/.git'                                                 \
+      --exclude='*/.svn'                                                 \
+      --exclude='*/.stack-work'                                          \
+      --exclude='*/bin'                                                  \
+      --exclude='*/obj'                                                  \
+      --exclude='media/cinematography'                                   \
+      /gahag 2>&1                                                        \
+    | tee $(date '+%Y-%m-%d(%H:%M:%S)-tar.log')
+}
+
+function gahag-backup-restore {
+  tar -xvpzf "/run/media/gahag/Backup/backup.$@.tar.gz" \
+      -C /gahag-restore \
+      --numeric-owner
 }
