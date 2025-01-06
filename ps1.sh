62a7b6dd70decd2eab10562d22657dc79060f740
Gabriel Bastos <gabriel.bastos@cloudwalk.io>
2025-01-06 15:33:07 -0300

    Add PS1 extensions

--- ps1.sh
+++ ps1.sh
@@ -7,47 +7,61 @@ case "$TERM" in
     ;;
 esac
 
+declare -A PS1_EXTS
+
 __prompt_command() {
   local EXIT="$?" # This needs to be first
-  
+
   local Reset='\[\e[0m\]'
-  
+
   local Red='\[\e[0;91m\]'
   local Gre='\[\e[0;32m\]'
   local Yel='\[\e[0;93m\]'
   local BBlu='\[\e[1;34m\]'
   local Pur='\[\e[0;35m\]'
-  
+
   local Corner1='\342\224\214'
 	local Corner2='\342\224\224'
 	local Dash='\342\224\200'
 	local Bar='\342\224\202'
-  
+
   if [ -n "$PROMPT_INIT" ]; then
     PS1="\n"
   else
     PS1=""
     PROMPT_INIT=yes
   fi
-  
+
+  # [exit_status]
   PS1+="${Corner1}${BBlu}[${Reset}"
-  
+
   if [ $EXIT != 0 ]; then
     PS1+="${Red}"
   else
     PS1+="${Gre}"
   fi
-  
+
   PS1+="$EXIT${BBlu}]${Reset}${Dash}"
-  
+
   if [ $EXIT -lt 10 ]; then
     PS1+="${Dash}"
   fi
-  
+
   if [ $EXIT -lt 100 ]; then
     PS1+="${Dash}"
   fi
-  
-  
-  PS1+="${BBlu}[${Reset}\u@\h${BBlu}]${Reset}${Dash}${Dash}${Dash}${BBlu}[${Yel}\w${BBlu}]\n${Reset}${Corner2}${Dash}${BBlu}λ${Reset} "
+
+  # [user:host]
+  PS1+="${BBlu}[${Reset}\u@\h${BBlu}]${Reset}"
+
+  # extensions
+  for key in "${!PS1_EXTS[@]}"; do  # [workdir]
+    PS1+="${Dash}${Dash}${Dash}${BBlu}[${Pur}$key:${PS1_EXTS[$key]}${BBlu}]"
+  done
+
+  # [workdir]
+  PS1+="${Dash}${Dash}${Dash}${BBlu}[${Yel}\w${BBlu}]"
+
+  # prompt
+  PS1+="\n${Reset}${Corner2}${Dash}${BBlu}λ${Reset} "
 }
